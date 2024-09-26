3bf96f2ead5b997d00c394f495e00dd7a80613a0
Luca Macis <lucamacis92@gmail.com>
2024-09-26 11:15:06 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,3 @@
-
-
 import dash
 import dash_bootstrap_components as dbc
 from dash import dcc, html
@@ -14,6 +12,7 @@ DATA_PATH = 'data/interim/data_ready/'
 
 # Initialize the Dash app with a Bootstrap theme
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+server = app.server
 
 # Helper function to load all CSVs and concatenate into a single DataFrame
 def load_all_data():
