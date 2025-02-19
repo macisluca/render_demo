0c33f620fb1e58c1207c15c39866602f7e8eade8
lucamacis92 <lucamacis92@gmail.com>
2025-02-19 01:54:34 +0100

    eddy added

--- layouts/navbar.py
+++ layouts/navbar.py
@@ -5,7 +5,7 @@ navbar = dbc.NavbarSimple(
     children=[
         dbc.DropdownMenu(
             children=[
-                dbc.DropdownMenuItem("Monitoring Events (NEW)", href="/monitoring/eddy"),
+                dbc.DropdownMenuItem("Monitoring EDDY Events", href="/monitoring/eddy"),
                 dbc.DropdownMenuItem("Monitoring ACLED", href="/monitoring/acled"),
                 dbc.DropdownMenuItem("Monitoring World Bank Data", href="/monitoring/worldbank"),
                 dbc.DropdownMenuItem("Monitoring Elections Calendar", href="/monitoring/elections"),
