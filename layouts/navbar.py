75315a0878cf31804e57ed5ae8a27e9309869741
lucamacis92 <lucamacis92@gmail.com>
2025-03-07 13:45:55 +0100

    navbar updated

--- layouts/navbar.py
+++ layouts/navbar.py
@@ -5,11 +5,12 @@ navbar = dbc.NavbarSimple(
     children=[
         dbc.DropdownMenu(
             children=[
-                dbc.DropdownMenuItem("Monitoring EDDY Events", href="/monitoring/eddy"),
-                dbc.DropdownMenuItem("Monitoring ACLED", href="/monitoring/acled"),
-                dbc.DropdownMenuItem("Monitoring World Bank Data", href="/monitoring/worldbank"),
-                dbc.DropdownMenuItem("Monitoring Elections Calendar", href="/monitoring/elections"),
+                dbc.DropdownMenuItem("Explore Events", href="/monitoring/acled"),
+                dbc.DropdownMenuItem("Explore Socio-Political Data", href="/monitoring/worldbank"),
+                dbc.DropdownMenuItem("Explore Elections", href="/monitoring/elections"),
+                dbc.DropdownMenuItem("Explore EDDY Events (Just a Sample)", href="/monitoring/eddy"),
             ],
+            align_end=True,
             nav=True,
             in_navbar=True,
             label="Monitoring",
@@ -19,13 +20,20 @@ navbar = dbc.NavbarSimple(
                 dbc.DropdownMenuItem("Forecasting Global", href="/forecasting/global"),
                 dbc.DropdownMenuItem("Forecasting per Country", href="/forecasting/country"),
             ],
+            align_end=True,
             nav=True,
             in_navbar=True,
             label="Forecasting",
         ),
     ],
-    brand="Dashboard",
+    brand="Deeplomacy",
     brand_href="/",
     color="primary",
     dark=True,
+    style={
+        'padding-top': '0.2rem',
+        'padding-bottom': '0.2rem',
+        'padding-left': '1rem',
+        'padding-right': '1rem'
+    }
 )
